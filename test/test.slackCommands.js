24de976565067a3219474171155012e209d0b960
orthagonal <orthagonal1@gmail.com>
2017-12-04 21:53:46 -0600

    only test slack-command

--- test/test.slackCommands.js
+++ test/test.slackCommands.js
@@ -14,33 +14,6 @@ tap.afterEach(async() => {
   await slackCommand.stop();
 });
 
-tap.test('rejects if token does not match', async (t) => {
-  const response = await slackCommand.server.inject({
-    method: 'POST',
-    url: '/',
-    payload: {
-      token: 'is wrong'
-    }
-  });
-  t.equal(response.statusCode, 401, '401 unauthorized status code when token rejected');
-  await slackCommand.stop();
-  t.end();
-});
-
-tap.test('notifies if there is no matching command', async(t) => {
-  const response = await slackCommand.server.inject({
-    method: 'POST',
-    url: '/',
-    payload: {
-      token: 'a token',
-      command: '/and_conquer'
-    }
-  });
-  t.equal(response.statusCode, 405, '405 when command is missing ');
-  await slackCommand.stop();
-  t.end();
-});
-
 tap.test('accepts and processes command registered as a function', async(t) => {
   slackCommand.server.registerSlackCommand('/test', (slackPayload) => {
     return 'hello';
