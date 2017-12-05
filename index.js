2eb7a8a26f516a4bbc0ffa02a07edadaa270fba6
orthagonal <orthagonal1@gmail.com>
2017-12-04 23:15:43 -0600

    register logr-flat

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const Hapi = require('hapi');
 const hapiSlackCommand = require('hapi-slack-command');
-
+const hapiLogr = require('hapi-logr');
 
 class SlackCommand {
   constructor(port, options) {
@@ -14,10 +14,22 @@ class SlackCommand {
   }
 
   async start() {
-    await this.server.register({
+    await this.server.register([{
       plugin: hapiSlackCommand,
       options: this.options
-    });
+    },
+    {
+      plugin: hapiLogr,
+      options: {
+        reporters: {
+          flat: {
+            reporter: 'logr-flat',
+            options: {}
+         }
+        }
+      }
+    }
+    ]);
     await this.server.start();
   }
 }
