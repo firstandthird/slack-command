aab87ced2ba1e7abd0e50c6b1615f836d1292f1c
Chris <orthagonal1@gmail.com>
2018-03-14 13:32:34 -0500

    will load commands soon

--- test/test.slackCommands.js
+++ test/test.slackCommands.js
@@ -2,17 +2,28 @@
 const tap = require('tap');
 const Rapptor = require('rapptor');
 
-let slackCommand;
-tap.beforeEach(async() => {
-  slackCommand = new Rapptor({});
+tap.test('accepts and processes command registered as a function', async(t) => {
+  const slackCommand = new Rapptor({});
   await slackCommand.start();
-});
-
-tap.afterEach(async() => {
+  slackCommand.server.registerSlackCommand('check', slackPayload => 'hello');
+  const response = await slackCommand.server.inject({
+    method: 'POST',
+    url: '/api/command',
+    payload: {
+      token: 'a token',
+      command: '/test',
+      text: 'check'
+    }
+  });
+  t.equal(response.statusCode, 200, '200 when token accepted ');
+  t.equal(response.result, 'hello', 'gets info back');
   await slackCommand.stop();
+  t.end();
 });
 
 tap.test('accepts and processes command registered as a function', async(t) => {
+  const slackCommand = new Rapptor({});
+  await slackCommand.start();
   slackCommand.server.registerSlackCommand('check', slackPayload => 'hello');
   const response = await slackCommand.server.inject({
     method: 'POST',
